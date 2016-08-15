d6779f3f72382fa66ebf39ca56f3b033b069c707
Jenny Yang <jeyang@jyang01.phage.bcgsc.ca>
2016-08-15 14:49:37 -0700

    Working version of enter and click methods

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-(function($) {
+
+
+function searchGene(){
+	var userInput = document.getElementById('geneEntry').value;
+    var url = "http://www.cbioportal.org/webservice.do?cmd=getGeneticProfiles&cancer_study_id=" + userInput;
+    $('#stage').load(url);
+}
+
+
+/*(function($) {
     $.fn.onEnter = function(func) {
         this.bind('keypress', function(e) {
             if (e.keyCode == 13){
@@ -8,13 +17,14 @@
         });               
         return this; 
      };
-})(jQuery);
+})(jQuery);*/
 
 $( function () {
     //console.log($("geneEntry"));
-    $("#geneEntry").onEnter( function() {
-    	var userInput = document.getElementById('geneEntry').value;
-    	var url = "http://www.cbioportal.org/webservice.do?cmd=getGeneticProfiles&cancer_study_id=" + userInput;
-        $('#stage').load(url);                       
-    });
+    $("#geneEntry").onEnter(searchGene);
 });
+
+$(function(){
+	$("#submitGene").click(searchGene);
+});
+
