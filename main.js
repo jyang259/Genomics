1f67e3ee827fb22f7171b53a1163730896106042
Jenny Yang <jeyang@jyang01.phage.bcgsc.ca>
2016-08-16 15:42:44 -0700

    dload to excel button and exporting libraries added

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 //search for input gene after "Enter" key is pressed
 $(document).keypress(function(e){
 	if(e.which == 13){
@@ -7,7 +8,7 @@ $(document).keypress(function(e){
 
 //search for input gene after "Submit" button is pressed
 $(function(){
-	$("#submit-gene").click(searchGene);
+	$('#submit-gene').click(searchGene);
 });
 
 //function for displaying api call results with input gene
@@ -15,4 +16,12 @@ function searchGene(){
 	var userInput = document.getElementById('gene-entry').value;
     var url = "http://www.cbioportal.org/webservice.do?cmd=getGeneticProfiles&cancer_study_id=" + userInput;
     $('#stage').load(url);
+    //after table is loaded, enable export to excel button
+    $('#export-to-xls').attr('disabled', false);  
+}
+
+function reset(){
+	$('#gene-entry').val('');
+	$('#stage').val('');
+	$('#export-to-xls').attr('disabled', true);
 }
