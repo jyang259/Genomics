d35fc59f4b960b953e367eeea14e3536581c49e0
Jenny Yang <jeyang@jyang01.phage.bcgsc.ca>
2016-08-17 15:21:34 -0700

    Start editing css

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
 $(function(){
-	$('#case-study-entry').focus();
+	$('#cancer-study-entry').focus();
 	//search for input gene after "Submit" button is pressed
-	$('#submit-case-study').click(searchGene);
+	$('#submit-cancer-study').click(searchCancerStudy);
 	//search for input gene after "Enter" key is pressed
-	$('#case-study-entry').on("keydown", function(e){
+	$('#cancer-study-entry').on("keydown", function(e){
 		if(e.keyCode == 13){
-			searchGene();
+			searchCancerStudy();
 		}
 	});
 	//check if textfield is empty --> enables reset
-	$('#case-study-entry').on("keyup", checkTextField);
+	$('#cancer-study-entry').on("keyup", checkTextField);
 	//if reset is pressed, reset search box and stage
 	$('#reset-search').click(reset);
 });
 
 //function for displaying api call results with input gene
-function searchGene(){
-	var userInput = document.getElementById('case-study-entry').value;
+function searchCancerStudy(){
+	var userInput = document.getElementById('cancer-study-entry').value;
     var url = "http://www.cbioportal.org/webservice.do?cmd=getGeneticProfiles&cancer_study_id=" + userInput;
     $('#stage').load(url);
     //after table is loaded, enable export to excel button
@@ -24,23 +24,25 @@ function searchGene(){
 }
 
 function checkTextField(){
-	if($('#case-study-entry').val().length > 0){
-		$('#submit-case-study').attr('disabled', false);
+	if($('#cancer-study-entry').val().length > 0){
+		$('#submit-cancer-study').attr('disabled', false);
 		$('#reset-search').attr('disabled', false);
 	}
 	else{
 		if(!$.trim($('#stage').html()).length){
 			$('#reset-search').attr('disabled', true);
 		}
-		$('#submit-case-study').attr('disabled', true);
+		$('#submit-cancer-study').attr('disabled', true);
 	}
 }
 
+//reset text boxes and stage
 function reset(){
-	$('#case-study-entry').val('');
-	$('#case-study-entry').focus();
+	$('#cancer-study-entry').val('');
+	$('#cancer-study-entry').focus();
 	document.getElementById('stage').innerHTML = "";
 	$('#export-to-xls').attr('disabled', true);
 	$('#reset-search').attr('disabled', true);
-	$('#submit-case-study').attr('disabled', true);
+	$('#submit-cancer-study').attr('disabled', true);
 }
+
